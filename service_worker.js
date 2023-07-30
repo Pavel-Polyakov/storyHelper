eb3ac1ef32f2ba4be8582d4979d661edec2ec764
Pavel Polyakov <polyakov.pavel.xyz@gmail.com>
2023-07-30 23:03:03 +0100

    Added bootstrap and polyfill into the cache

--- service_worker.js
+++ service_worker.js
@@ -2,7 +2,12 @@
 self.addEventListener("install",e=>{
   e.waitUntil(
     caches.open("static").then(cache=>{
-      return cache.addAll(["./",'./images/logo_192.png']);
+      return cache.addAll([
+        "./",
+        './images/logo_192.png',
+        "https://cdn.jsdelivr.net/npm/context-filter-polyfill/dist/index.min.js",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+      ]);
     })
   );
 });
